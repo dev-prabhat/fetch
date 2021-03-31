6eba139fb2e628fc4cd8a71ea90a398c1792ed2d
Prabhat <singhprabhatp.s.224@gamil.com>
2021-03-31 16:24:29 +0530

    All

--- fetch.js
+++ fetch.js
@@ -4,6 +4,15 @@ app.use(express.json());
 PORT = 8080;
 
 app.get('/api/branches/autocomplete',(req,res)=>{
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+
+  const startIndex = (page-1)*limit;
+  const endIndex = page*limit;
+
+  const branches = {}
+  
+  branches.result = autocomplete.slice(startIndex,endIndex)
  res.json(autocomplete);
 })
 
