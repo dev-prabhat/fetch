2d734eac936d34afeb17638f37b251c513b77318
Prabhat <singhprabhatp.s.224@gamil.com>
2021-03-31 16:01:29 +0530

    All

--- Node_Backend/fetch.js
+++ Node_Backend/fetch.js
@@ -3,15 +3,15 @@ const app = express();
 app.use(express.json());
 PORT = 8080;
 
-app.get('/api/branches/autocomplete?q=RTGS&limit=3&offset=0',(req,res)=>{
- res.send(autocomplete);
+app.get('/api/branches/autocomplete',(req,res)=>{
+ res.json(autocomplete);
 })
 
-app.get('/api/branches/:city&limit=4&offset=0',(req,res)=>{
+app.get('/api/branches/:city',(req,res)=>{
   const branch = branches.find( c => c.city === JSON.stringify(req.params.city))
 
   if(!branch)  return res.status(404).json({error:"Branch Not Found "})
-  res.send(branch);
+  res.json(branch);
 })
 
 app.listen(PORT,()=>{
